ea6ce9ff9dc242b07abb3cf512ff04b4785c1f97
IMPraveenRaj <impraveen13@gmail.com>
2024-11-04 08:30:12 +0100

    security-pro-changes

---
+++ security-pro/src/main/java/com/springheaven/securityapp/controller/HiController.java
@@ -0,0 +1,16 @@
+package com.springheaven.securityapp.controller;
+
+public class HiController {
+	
+	public String sayHi() {
+		return "say Hi";
+	}
+	
+	
+	
+
+	public String sayBye() {
+		return "say Hi";
+	}
+
+}
