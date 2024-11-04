27dd8a292bd649bf88aaead0ac2351a78b59395a
IMPraveenRaj <impraveen13@gmail.com>
2024-11-04 15:39:31 +0100

    springsecurity user authentication manager changes

--- security-pro/src/main/java/com/springheaven/securityapp/controller/config/MyAppSecurityInitializer.java
+++ security-pro/src/main/java/com/springheaven/securityapp/controller/config/MyAppSecurityInitializer.java
@@ -1,7 +1,10 @@
 package com.springheaven.securityapp.controller.config;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
+//this class will initialize and protect your resource by enable the security but this class will 
+// internally look for the bean name called spring security filter chain that will be created by enabling 
+//@Enablewebsecurity from spring-security-config dependencies that will create a security filter chain 
+//it will be used by the this abstractsecuritywebapplicationinitilizer
 public class MyAppSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
 
 }
--- security-pro/src/main/java/com/springheaven/securityapp/controller/config/SecurityAppConfig.java
+++ security-pro/src/main/java/com/springheaven/securityapp/controller/config/SecurityAppConfig.java
@@ -1,10 +1,46 @@
 package com.springheaven.securityapp.controller.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity(debug = false)
 @Configuration
 public class SecurityAppConfig {
+
+	// whenever we are implementing any security we need to validate again the users
+	// we have in db or anywhere we are creating the user for this application to
+	// access in this down below and the created user has to be stored in the
+	// database
+	@Bean
+	public InMemoryUserDetailsManager userDetails() {
+
+		List<SimpleGrantedAuthority> grantedauthority = new ArrayList<>();
+		grantedauthority.add(new SimpleGrantedAuthority("user"));
+		grantedauthority.add(new SimpleGrantedAuthority("admin"));
+		UserDetails praveenUser = new User("praveenraj", "praveenraj", grantedauthority);
+		InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
+		inMemoryUserDetailsManager.createUser(praveenUser);
+		return inMemoryUserDetailsManager;
+
+	}
+
+	/*
+	 * AuthenticationManager authenticationManager() { return }
+	 */
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
 
 }
